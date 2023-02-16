aba99b9da16ddfea9dc562b7b003596ead6fb243
Mark Reckard <mark.reckard@gmail.com>
2023-02-15 21:52:42 -0500

    black

--- shared_items/utils/__init__.py
+++ shared_items/utils/__init__.py
@@ -1,1 +1,7 @@
-from .utils import pp, reversor, convert_runtime, strip_all_punctuation, measure_execution
+from .utils import (
+    pp,
+    reversor,
+    convert_runtime,
+    strip_all_punctuation,
+    measure_execution,
+)
--- shared_items/utils/utils.py
+++ shared_items/utils/utils.py
@@ -6,33 +6,39 @@ from typing import Optional
 
 
 class reversor:
-  def __init__(self, obj):
-    self.obj = obj
+    def __init__(self, obj):
+        self.obj = obj
 
-  def __eq__(self, other):
-    return other.obj == self.obj
+    def __eq__(self, other):
+        return other.obj == self.obj
 
-  def __lt__(self, other):
-    return other.obj < self.obj
+    def __lt__(self, other):
+        return other.obj < self.obj
+
 
 pp = pprint.PrettyPrinter().pprint
 
+
 def convert_runtime(runtime: int) -> str:
-  return f'{int(runtime / 60)}:{(runtime % 60):02d}'
+    return f"{int(runtime / 60)}:{(runtime % 60):02d}"
+
 
 def strip_all_punctuation(subject: str) -> str:
-  # add "smart quotes" that Notion uses
-  punctuation = string.punctuation + '“‘”’'
-  return subject.translate(str.maketrans('', '', punctuation))
+    # add "smart quotes" that Notion uses
+    punctuation = string.punctuation + "“‘”’"
+    return subject.translate(str.maketrans("", "", punctuation))
+
 
 # todo: make this take an arg that describes what is happening in the block ie "fetching results..."
 # https://realpython.com/primer-on-python-decorators/#both-please-but-never-mind-the-bread
-def measure_execution(func, ):
-  @functools.wraps(func)
-  def wrapper(*args, **kwargs):
-    start_time = time.time()
-    res = func(*args, **kwargs)
-    print(f'Took {time.time() - start_time} seconds')
-    return res
+def measure_execution(
+    func,
+):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        res = func(*args, **kwargs)
+        print(f"Took {time.time() - start_time} seconds")
+        return res
 
-  return wrapper
+    return wrapper
