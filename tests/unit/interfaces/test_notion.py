659794eec581cbb68c5175dbe64689b1d3f107e7
Mark Reckard <mark.reckard@gmail.com>
2023-02-15 22:03:39 -0500

    add tests

---
+++ tests/unit/interfaces/test_notion.py
@@ -0,0 +1,31 @@
+from shared_items.interfaces.notion import Notion, Prop as NotionProp
+
+
+def test_assemble_prop_handles_rich_text():
+  notion = Notion()
+
+  dummy_prop: NotionProp = {
+    'name': 'SomeName',
+    'type': 'rich_text',
+    'content': 'yadayada'
+  }
+
+  dummy_output = notion.assemble_prop(dummy_prop)
+  expected_output = {'rich_text': [{'text': {'content': 'yadayada'}}]}
+
+  assert dummy_output == expected_output
+
+
+def test_assemble_props_handles_rich_text():
+  notion = Notion()
+
+  dummy_prop: NotionProp = {
+    'name': 'SomeName',
+    'type': 'rich_text',
+    'content': 'yadayada'
+  }
+
+  dummy_output = notion.assemble_props([dummy_prop])
+  expected_output = {'SomeName': {'rich_text': [{'text': {'content': 'yadayada'}}]}}
+
+  assert dummy_output == expected_output
