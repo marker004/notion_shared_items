0510a0dc9b172dbc626e15fef79517b4ec16eb37
Mark Reckard <mark.reckard@gmail.com>
2023-02-24 23:29:44 -0500

    black tests

--- tests/unit/interfaces/test_notion.py
+++ tests/unit/interfaces/test_notion.py
@@ -2,30 +2,30 @@ from shared_items.interfaces.notion import Notion, Prop as NotionProp
 
 
 def test_assemble_prop_handles_rich_text():
-  notion = Notion()
+    notion = Notion()
 
-  dummy_prop: NotionProp = {
-    'name': 'SomeName',
-    'type': 'rich_text',
-    'content': 'yadayada'
-  }
+    dummy_prop: NotionProp = {
+        "name": "SomeName",
+        "type": "rich_text",
+        "content": "yadayada",
+    }
 
-  dummy_output = notion.assemble_prop(dummy_prop)
-  expected_output = {'rich_text': [{'text': {'content': 'yadayada'}}]}
+    dummy_output = notion.assemble_prop(dummy_prop)
+    expected_output = {"rich_text": [{"text": {"content": "yadayada"}}]}
 
-  assert dummy_output == expected_output
+    assert dummy_output == expected_output
 
 
 def test_assemble_props_handles_rich_text():
-  notion = Notion()
+    notion = Notion()
 
-  dummy_prop: NotionProp = {
-    'name': 'SomeName',
-    'type': 'rich_text',
-    'content': 'yadayada'
-  }
+    dummy_prop: NotionProp = {
+        "name": "SomeName",
+        "type": "rich_text",
+        "content": "yadayada",
+    }
 
-  dummy_output = notion.assemble_props([dummy_prop])
-  expected_output = {'SomeName': {'rich_text': [{'text': {'content': 'yadayada'}}]}}
+    dummy_output = notion.assemble_props([dummy_prop])
+    expected_output = {"SomeName": {"rich_text": [{"text": {"content": "yadayada"}}]}}
 
-  assert dummy_output == expected_output
+    assert dummy_output == expected_output
--- tests/unit/utils/test_utils.py
+++ tests/unit/utils/test_utils.py
@@ -1,22 +1,25 @@
 from shared_items.utils import convert_runtime, strip_all_punctuation
 
-def test_convert_runtime_converts_minutes_to_proper_format_normal():
-  runtime = 152
-  output = convert_runtime(runtime)
-  expected_output = '2:32'
 
-  assert output == expected_output
+def test_convert_runtime_converts_minutes_to_proper_format_normal():
+    runtime = 152
+    output = convert_runtime(runtime)
+    expected_output = "2:32"
+
+    assert output == expected_output
+
 
 def test_convert_runtime_converts_minutes_to_proper_format_zero():
-  runtime = 0
-  output = convert_runtime(runtime)
-  expected_output = '0:00'
+    runtime = 0
+    output = convert_runtime(runtime)
+    expected_output = "0:00"
 
-  assert output == expected_output
+    assert output == expected_output
+
 
 def test_strip_all_punctuation():
-  test_string = 'abc123!@#$%^&*()_~":\'?><,.“‘”’'
-  output = strip_all_punctuation(test_string)
-  expected_output = 'abc123'
+    test_string = "abc123!@#$%^&*()_~\":'?><,.“‘”’"
+    output = strip_all_punctuation(test_string)
+    expected_output = "abc123"
 
-  assert output == expected_output
+    assert output == expected_output
